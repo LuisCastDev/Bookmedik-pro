44e45676ad10bda4f41bd3aeca3ca7b985b13762
luiisCastillo <90583642+LuisCastDev@users.noreply.github.com>
2024-01-14 23:53:14 -0400

    Trabajando en printear el pdf

--- core/app/action/updatereservation-action.php
+++ core/app/action/updatereservation-action.php
@@ -1,45 +1,13 @@
+
 <?php
-require('/xampp/htdocs/Bookmedik-pro/fpdf/fpdf.php');
+	require('/xampp/htdocs/Bookmedik-pro/fpdf/fpdf.php');
 
-$pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial', 'B', 9);
-$pdf->Cell(300, 60, $_POST["medicaments"]);
-
-$pdfContent = $pdf->Output('', 'S');
-
-$html = <<<HTML
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Impresión del PDF</title>
-</head>
-<body>
-    <script>
-        // Función para abrir una nueva ventana con el cuadro de diálogo de impresión
-        function imprimirPDF() {
-            var ventanaImpresion = window.open('', '_blank');
-            ventanaImpresion.document.write('<html><head><title>Imprimir PDF</title></head><body>');
-            ventanaImpresion.document.write('<embed width="100%" height="100%" type="application/pdf" src="data:application/pdf;base64,' + btoa('{$pdfContent}') + '">');
-            ventanaImpresion.document.write('</body></html>');
-            ventanaImpresion.document.close();
-            ventanaImpresion.print();
-        }
-
-        // Llamar a la función para abrir la nueva ventana con el cuadro de diálogo de impresión
-        imprimirPDF();
-    </script>
-</body>
-</html>
-HTML;
-
-// Imprimir el contenido HTML
-echo $html;
-?>
-<?php
-
+	$pdf = new FPDF();
+	$pdf->AddPage();
+	$pdf->SetFont('Arial', 'B', 9);
+	$pdf->Cell(300, 60, $_POST["medicaments"]);
+	
+	$pdf->Output('Receta.pdf', 'I');
 if(count($_POST)>0){
 	$user = ReservationData::getById($_POST["id"]);
 	$user->no = $_POST["no"];
@@ -62,7 +30,10 @@ if(count($_POST)>0){
 Core::alert("Actualizado exitosamente!");
 
 if(isset($_SESSION["medic_id"])){
+
 print "<script>window.location='index.php?view=medicreservations';</script>";
+
+
 
 }else if(isset($_SESSION["user_id"])){
 print "<script>window.location='index.php?view=reservations';</script>";
